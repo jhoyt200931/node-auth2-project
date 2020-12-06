02f2131da081a8cd941967cc6d8ecd73da457576
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-12-06 17:31:28 -0500

    created restriction middleware and implemented get endpoint with restriction for the users route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const authRouter = require('./api/authRouter.js');
-
+const usersRouter = require('./api/usersRouter.js');
 
 const server = express();
 
@@ -9,6 +9,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
 
 
 
